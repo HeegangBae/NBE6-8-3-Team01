575e8e8cb94705832ca6522c6b177ffc55e70ec2
heigangbae <j56233145@gmail.com>
2025-09-03 16:26:01 +0900

    t15

--- backend/src/main/java/com/bookbook/BookbookApplication.kt
+++ backend/src/main/java/com/bookbook/BookbookApplication.kt
@@ -1,7 +1,9 @@
 package com.bookbook
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationListener
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
@@ -13,5 +15,9 @@ class BookbookApplication
 fun main(args: Array<String>) {
     val dbUrl = System.getenv("DATABASE_URL")
     println("DEBUG: DATABASE_URL is $dbUrl")
-    runApplication<BookbookApplication>(*args)
+    runApplication<BookbookApplication>(*args) {
+        addListeners(ApplicationListener<ApplicationEnvironmentPreparedEvent> { event ->
+            println("DEBUG: Active Profiles: ${event.environment.activeProfiles.joinToString()}")
+        })
+    }
 }
